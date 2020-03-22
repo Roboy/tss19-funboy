d80b537ac15d4089dc20a91635bede4f911ecfc3
Wagram Airiian <vagram.airiian@tum.de>
2020-03-22 23:51:22 +0100

    Small fix

--- src/rosemo/install.sh
+++ src/rosemo/install.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
-mkdir /home/wagram/data
-mkdir /home/wagram/sdata
+mkdir /home/roboy/data
+mkdir /home/roboy/sdata
 docker build -t rosemo .
-docker run --name rosemo -p "11311:11311" -p "33690:33690" -v /home/wagram/data:/rosemo/data -v /home/wagram/sdata:/rosemo/sdata -v /home/wagram/tss19-funboy/src/rosemo/ros:/catkin_ws/src/rosemo --device /dev/video0:/dev/video0 --device /dev/snd:/dev/snd -d rosemo
+docker run --name rosemo -p "11311:11311" -p "33690:33690" -v /home/roboy/data:/rosemo/data -v /home/roboy/sdata:/rosemo/sdata -v /home/roboy/tss19-funboy/src/rosemo/ros:/catkin_ws/src/rosemo --device /dev/video0:/dev/video0 --device /dev/snd:/dev/snd -d rosemo
 
